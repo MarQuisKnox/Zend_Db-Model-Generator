147ab86e51953e9a792ba08992c6525ea1133ee8
kfirufk@gmail.com <kfirufk@gmail.com@f5331d8a-66fc-11de-b0a5-b72b347d2ad3>
2009-09-20 16:58:57 +0000

    also updated readme file on last commit. removing die() that i used for debugging in order for the script to work again.
    git-svn-id: http://zend-db-model-generator.googlecode.com/svn/trunk@19 f5331d8a-66fc-11de-b0a5-b72b347d2ad3

--- zend-db-model-generator.php
+++ zend-db-model-generator.php
@@ -17,7 +17,7 @@ class MakeDbTable {
 	protected $_dbhost='127.0.0.1';  // database host
 	protected $_dbtype='mysql';    // database type
 	protected $_dbuser='root';       // database user
-	protected $_dbpassword='qweasd'; // database password
+	protected $_dbpassword=''; // database password
 	protected $_addRequire=false;   // to add require_once to in order to include the relevant php files. usful if you don't have class auto-loading. if you're using Zend Framework's MVC you can probably set this to false  
 
 
@@ -62,8 +62,7 @@ class MakeDbTable {
 		
 			$columns[]=$row['Field'];
 			if ($row['Key'] == 'PRI')
-                            die(var_export($row,1));
-				//$primaryKey[]=$row['Field'];
+				$primaryKey[]=$row['Field'];
 		}
 
 		if (sizeof($primaryKey) == 0) {
