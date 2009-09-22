1eb260a602277c48432a454762185bb65e95da7c
kfirufk@gmail.com <kfirufk@gmail.com@f5331d8a-66fc-11de-b0a5-b72b347d2ad3>
2009-09-22 13:45:40 +0000

    version 0.1.4
    git-svn-id: http://zend-db-model-generator.googlecode.com/svn/trunk@21 f5331d8a-66fc-11de-b0a5-b72b347d2ad3

--- zend-db-model-generator.php
+++ zend-db-model-generator.php
@@ -1,6 +1,6 @@
 <?php
 
-$version="0.1.3a";
+$version="0.1.4";
 $author="Kfir Ozer <kfirufk@gmail.com>";
 
 class MakeDbTable {
@@ -17,8 +17,8 @@ class MakeDbTable {
 	protected $_dbhost='127.0.0.1';  // database host
 	protected $_dbtype='mysql';    // database type
 	protected $_dbuser='root';       // database user
-	protected $_dbpassword='qweasd'; // database password
-	protected $_addRequire=true;   // to add require_once to in order to include the relevant php files. usful if you don't have class auto-loading. if you're using Zend Framework's MVC you can probably set this to false  
+	protected $_dbpassword=''; // database password
+	protected $_addRequire=false;   // to add require_once to in order to include the relevant php files. usful if you don't have class auto-loading. if you're using Zend Framework's MVC you can probably set this to false  
 
 
 	private function _getCapital($str) {
